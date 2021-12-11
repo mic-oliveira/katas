109fcedbda88fa4fed957f1b3f556d9ff12aca05
guilherme637 <guilhermejr296@icloud.com>
2021-12-11 16:30:53 -0300

    implementando primeiro teste

--- calculadora/src/Calculadora.php
+++ calculadora/src/Calculadora.php
@@ -4,8 +4,12 @@ namespace Kata\Calculadora;
 
 class Calculadora
 {
-
     public function __construct()
     {
+    }
+
+    public function add(float $primeiroValor, float $segundoValor): float
+    {
+        return 8;
     }
 }
