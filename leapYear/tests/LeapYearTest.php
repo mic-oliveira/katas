9378f268bf7c4378c8a60f0ec3b6099b40004d23
guilherme637 <guilhermejr296@icloud.com>
2021-12-11 17:17:33 -0300

    teste

--- leapYear/tests/LeapYearTest.php
+++ leapYear/tests/LeapYearTest.php
@@ -2,9 +2,14 @@
 
 namespace Kata\LeapYear\Tests;
 
+use Kata\LeapYear\LeapYear;
 use PHPUnit\Framework\TestCase;
 
 class LeapYearTest extends TestCase
 {
-
+    public function should_leap_year()
+    {
+        $leapYear = new LeapYear();
+        $this->assertFalse($leapYear->isLeapYear(1997));
+    }
 }
