1e651919d5997258bae682101c5e10b9e3e32ce1
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-12-11 18:15:09 -0300

    finalizando leap year

--- leapYear/src/LeapYear.php
+++ leapYear/src/LeapYear.php
@@ -4,17 +4,35 @@ namespace Kata\LeapYear;
 
 class LeapYear
 {
+
     public function isLeapYear(int $year): bool
     {
         if (!$this->isDivisibleByFour($year)) {
             return false;
         }
+        if ($this->isDivisibleByHundredAndNotFourHundred($year))
+            return false;
         return true;
     }
 
     public function isDivisibleByFour(int $value): bool
     {
-        return ($value % 4) == 0;
+        return ($value % 4) === 0;
+    }
+
+    public function isDivibleByHundred(int $value): bool
+    {
+        return ($value % 100) === 0;
+    }
+
+    public function isDivisibleByFourHundred($value): bool
+    {
+        return ($value % 400) === 0;
+    }
+
+    public function isDivisibleByHundredAndNotFourHundred($value): bool
+    {
+        return $this->isDivibleByHundred($value) && !$this->isDivisibleByFourHundred($value);
     }
 
 }
--- leapYear/tests/LeapYearTest.php
+++ leapYear/tests/LeapYearTest.php
@@ -7,11 +7,23 @@ use PHPUnit\Framework\TestCase;
 
 class LeapYearTest extends TestCase
 {
-    public function test_should_leap_year()
+    /**
+     * @dataProvider years
+     */
+    public function test_should_leap_year($year, $expected)
     {
         $leapYear = new LeapYear();
-        $this->assertFalse($leapYear->isLeapYear(1997));
-        $this->assertTrue($leapYear->isLeapYear(1996));
-        $this->assertTrue($leapYear->isLeapYear(1600));
+        $this->assertEquals($expected, $leapYear->isLeapYear($year));
     }
+
+    public function years(): array
+    {
+        return [
+            [1997, false],
+            [1996, true],
+            [1600, true],
+            [1800, false]
+        ];
+    }
+
 }
