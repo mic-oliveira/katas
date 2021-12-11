5d4efe6adadb613d4da1a5624e0f4193740a71a2
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-12-11 15:51:37 -0300

    kata calculadora

---
+++ calculadora/tests/CalculadoraTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Kata\Calculadora\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculadoraTest extends TestCase
+{
+    public function test_should_add()
+    {
+        $this->assertTrue(false);
+    }
+}
